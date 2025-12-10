1c2df7291f43cfd13ea8339df1d207c2abe1c764
Moamen Elsharawy <moamenelsharawy2004@gmail.com>
2025-12-10 05:03:00 +0200

    Hospital, Doctor V2

---
+++ Serena.DAL/Migrations/20251208142555_InitialCreate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Serena.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "price",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScheduleId",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ScheduleId",
+                table: "Appointments",
+                column: "ScheduleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Schedules_ScheduleId",
+                table: "Appointments",
+                column: "ScheduleId",
+                principalTable: "Schedules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Schedules_ScheduleId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_ScheduleId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduleId",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Appointments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "price",
+                table: "Appointments",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
